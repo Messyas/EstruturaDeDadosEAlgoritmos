429b3b493ee39fe68f1fdffb108cbf0351e76260
Messyas <messyasgoisfranca@gmail.com>
2025-12-20 11:34:48 -0400

    ex2

---
+++ ex02.c
@@ -0,0 +1,18 @@
+//Exibir o produto dos numeros inteiros no intervalo de um a cem
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    double produto = 1.0;
+    int i = 1;
+    while (i <= 100) {
+        produto = produto * i;
+        i++;
+    }
+    printf("%e", produto);
+    // poderia ser ld (long float que e como e compilado)
+    // no caso com int nao seria conpilador por causa do limite de memoria.
+
+    return 0;
+}
