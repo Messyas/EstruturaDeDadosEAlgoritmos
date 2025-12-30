99dfb505793477719f9ad4d0a1a84037cf043db5
Messyas <messyasgoisfranca@gmail.com>
2025-12-30 19:54:02 -0400

    organizando

---
+++ ListaDeAlgorimosResolvidos/ex014.c
@@ -0,0 +1,21 @@
+\*
+Fazer um programa que leia 10 números inteiros e armazene-os em um vetor. 
+O programa deve ordenar esse vetor em ordem crescente 
+(do menor para o maior) e imprimir o vetor ordenado.
+
+Exemplo: Entrada [5, 1, 9] -> Saída [1, 5, 9]
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void bubbkeSort(int *array, int size) {
+   
+}
+
+int main () {
+  
+   
+return 0;
+}
---
+++ ListaDeAlgorimosResolvidos/ex015.c
@@ -0,0 +1,11 @@
+/**
+15. Faça um programa que gerencie um cadastro de 5 livros. Crie uma struct chamada 
+Livro com: Título (string), Autor (string) e Ano (int).
+
+O programa deve:
+Ler os dados de 5 livros e guardar num vetor de structs.
+Pedir para o usuário digitar um ano.
+Buscar e imprimir todos os livros que foram lançados neste ano.
+Se não achar nenhum, avise.
+
+*/
