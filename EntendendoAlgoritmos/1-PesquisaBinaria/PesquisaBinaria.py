37f7e48ed3658f62b19458bd0811ad9f908c01f0
Messyas <messyasgoisfranca@gmail.com>
2026-01-19 13:50:45 -0400

    Pesquisa binaria

---
+++ EntendendoAlgoritmos/1-PesquisaBinaria/PesquisaBinaria.py
@@ -0,0 +1,19 @@
+def pesquisa_binaria(lista, item):
+    baixo = 0
+    alto =  len(lista) - 1
+    while baixo < alto:
+        meio = (baixo + alto) / 2
+        chute = lista[meio]
+        if chute == item:
+            return meio
+        if chute > item:
+            alto = meio - 1
+        else:
+            baixo = meio + 1
+    return None
+        
+lista_ordernada = [1,2,3,4,5,6,7,8,9,10]
+item_pesquisado = 9
+
+item = pesquisa_binaria(lista_ordernada, item_pesquisado)
+print(item)
