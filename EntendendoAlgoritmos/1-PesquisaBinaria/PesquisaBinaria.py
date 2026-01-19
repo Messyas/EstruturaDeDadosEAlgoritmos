1c830a067074baf6d2949b4115488bbbf4dd0108
Messyas <messyasgoisfranca@gmail.com>
2026-01-19 13:53:22 -0400

    Correcao de sintax na no algoritimo de pesquisa binaria.

--- EntendendoAlgoritmos/1-PesquisaBinaria/PesquisaBinaria.py
+++ EntendendoAlgoritmos/1-PesquisaBinaria/PesquisaBinaria.py
@@ -1,8 +1,8 @@
 def pesquisa_binaria(lista, item):
     baixo = 0
     alto =  len(lista) - 1
-    while baixo < alto:
-        meio = (baixo + alto) / 2
+    while baixo <= alto:
+        meio = (baixo + alto) // 2
         chute = lista[meio]
         if chute == item:
             return meio
