642b9e531b630d7d20c7a1c41408ad2d90fe3488
Messyas <messyasgoisfranca@gmail.com>
2026-01-28 17:37:17 -0400

    aula 27

---
+++ PPGI/aula27_busca_binaria.c
@@ -0,0 +1,15 @@
+/*
+
+*/
+
+#include <stdio.h>
+#include <string.h> 
+
+//ache primeiro o ponto medio, medio = (inicio + fim)/ 2 para achar o meio do vetor
+int buscarBinaria() {
+
+}
+
+int main() {
+    
+}
