dcc8b10ae9b8e13a8a6c8a536d5e9887baaa04c2
Messyas <messyasgoisfranca@gmail.com>
2026-02-02 22:41:50 -0400

    algorimos gulosos

---
+++ EntendendoAlgoritmos/8-AlgoritmosGulosos/algoritmos_gulosos.py
@@ -0,0 +1,43 @@
+estados_abranger = set(["mt", "wa", "or", "id", "nv", "ut", "ca", "az"])
+
+arr = [1, 2, 2, 3, 3, 3]
+
+set(arr)
+set([1, 2, 3])
+
+estacoes = {}
+estacoes["kum"] = set(["id", "nv", "ut"])
+estacoes["kdois"] = set(["wa", "id", "mt"])
+estacoes["ktres"] = set(["or", "nv", "ca"])
+estacoes["kquatro"] = set(["nv", "ut"])
+estacoes["kcinco"] = set(["ca", "az"])
+
+estacoes_final = set()
+
+melhor_estacao = None
+estados_cobertos = set()
+for estacao, estados_por_estacao in estacoes.items():
+    cobertos = estados_abranger & estados_por_estacao
+    if len(cobertos) > len(estados_cobertos):
+        melhor_estacao = estacao
+        estados_cobertos = cobertos
+
+estacoes_finais.add(melhor_estacao)
+
+estados_abranger -= estados_cobertos
+
+while estados_abranger:
+    melhor_estacao = None
+    estados_cobertos = set()
+    for estacao, estados in estacoes.items():
+        cobertos = estados_abranger & estados
+        if len(cobertos) > len(estados_cobertos):
+            melhor_estacao = estacao
+            estados_cobertos = cobertos
+    
+    estados_abranger -= estados_cobertos
+    estacos_finais.add(melhor_estacao)
+
+    print(estacoes_finais)
+
+set(['ktwo', 'kthree', 'kone', 'kfive'])
